eb7b9e926a8f5e33ae9ec25a4f73e9ca849994dc
Patrick Reynolds <patrickscottreynolds@gmail.com>
2014-09-24 10:41:17 -0700

    Remove local MONGO_URI environment variable, and need for .env file.

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 var express       = require('express'),     // Express Framework
     mongoose      = require('mongoose'),    // MongoDB Mapping
     bodyParser    = require('body-parser'), // Request body parser
-    dotenv        = require('dotenv'),      // Hiding environment variables
     config        = require('./config'),    // Config options
     errorhandler  = require('errorhandler'),// Stack trace printing on error
     app           = express();              // Creating the express app
 
+// Creating MongoDB connection via Mongoose
+mongoose.connect(config.database.mongo_uri);
+
 // Conditional module loading if in development environment
 if ('development' == app.get('env')) {
   app.use(errorhandler());
-  dotenv.load();
 };
-
-// Creating MongoDB connection via Mongoose
-mongoose.connect(config.database().mongo_uri);
 
 // Setting bodyParser as express' parser
 app.use(bodyParser.json());
@@ -26,6 +24,7 @@ var routes = require('./routes')(app);
 
 // Set the application to listen on a specified port
 app.listen(app.get("port"), function() {
-  console.log("Checklist API is listening on port " + app.get("port") +
-    " in " + app.settings.env + " mode");
+  var status = "Checklist API is listening on port " + app.get("port") +
+               " in " + app.settings.env + " mode"
+  console.log(status);
 });
--- config/database.js
+++ config/database.js
@@ -1,18 +1,16 @@
-module.exports = function() {
+module.exports = (function() {
   // Setting local NODE_ENV to production in heroku environment
-  switch (process.env.NODE_ENV || "development") {
-    case "production":
+  switch (process.env.NODE_ENV || 'development') {
+    case 'production':
       return {
         // Setting MONGO_URI to the MongoLab Database
         // http://mongolab.com
         mongo_uri: process.env.MONGO_URI
       };
-    case "development":
+    case 'development':
       return {
-        // Use local system MongoDB
-        // Typically someting along the lines of the following
-        // mongodb://localhost/<database name>
-        mongo_uri: process.env.LOCAL_MONGO_URI
+        // Use local MongoDB database
+        mongo_uri: 'mongodb://localhost/checklist_api'
       };
   }
-};
+})();
